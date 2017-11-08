9ae82099e507f8bf22f70ba4945fe30f0365e473
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2017-11-08 14:35:00 -0500

    Sentry

--- index.ios.js
+++ index.ios.js
@@ -1,1 +1,6 @@
 import './src';
+
+import { Sentry } from 'react-native-sentry';
+
+Sentry.config("https://dbf81363fedc4627b0b3ae7bbcb5625a:210318a36a20460ba574ab573c7fc483@sentry.io/242049").install();
+
