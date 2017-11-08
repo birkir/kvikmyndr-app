b68ebfdb484df1170f5079e115afbbf27f1ae795
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2017-11-07 19:46:48 -0500

    Small update

--- src/store/Auth.js
+++ src/store/Auth.js
@@ -1,24 +1,12 @@
 import { observable, action, computed } from 'mobx';
 import { persist } from 'mobx-persist';
-// import Auth0 from 'react-native-auth0';
-// import config from '../config';
-
-// const auth0 = new Auth0({
-//   domain: config.AUTH0_DOMAIN,
-//   clientId: config.AUTH0_CLIENT_ID,
-// });
 
 export default class Auth {
 
   @action
   async signin() {
     try {
-      // const token = await auth0.webAuth.authorize({
-      //   scope: 'openid email',
-      //   audience: `https://${config.AUTH0_DOMAIN}/userinfo`,
-      // });
       this.error = null;
-      // this.token = token;
     } catch (err) {
       this.error = err;
     }
