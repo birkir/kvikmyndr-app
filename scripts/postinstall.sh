f00420819db8912ce2e538e0ad44832b4dd0c60f
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2018-08-29 18:25:10 -0400

    Move this too

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,8 +1,7 @@
-./scripts/build-env.sh
-
 rm -rf ./node_modules/react-native-navigation/lib/android/app/src/main/java/com/reactnativenavigation/react/NavigationReactNativeHost.java
 
 # Pod install
 if [ -z "$APPCENTER_BUILD_ID" ]; then
+  ./scripts/build-env.sh
   (cd ios; pod install; cd -)
 fi
