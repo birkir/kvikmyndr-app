03b4a6b3d6a2886675c3771daab26680fea2161e
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2018-08-29 18:24:43 -0400

    Lets try this

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,6 +3,6 @@
 rm -rf ./node_modules/react-native-navigation/lib/android/app/src/main/java/com/reactnativenavigation/react/NavigationReactNativeHost.java
 
 # Pod install
-if [ -z "$TRAVIS" ]; then
+if [ -z "$APPCENTER_BUILD_ID" ]; then
   (cd ios; pod install; cd -)
 fi
