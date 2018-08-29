850120a5e7d3e0acb0fd2a7bb5e0ee1ebe65d3da
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2018-08-29 18:36:43 -0400

    Postinstall pod install repo update

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,7 +1,10 @@
+./scripts/build-env.sh
+
 rm -rf ./node_modules/react-native-navigation/lib/android/app/src/main/java/com/reactnativenavigation/react/NavigationReactNativeHost.java
 
 # Pod install
 if [ -z "$APPCENTER_BUILD_ID" ]; then
-  ./scripts/build-env.sh
   (cd ios; pod install; cd -)
+else
+  (cd ios; pod install --repo-update; cd -)
 fi
