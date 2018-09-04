5fb6b552cd53eeb89d6fcc465166d8d6d61a0bea
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2018-09-04 11:34:07 -0400

    Bundling with Sentry

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,8 +3,6 @@
 rm -rf ./node_modules/react-native-navigation/lib/android/app/src/main/java/com/reactnativenavigation/react/NavigationReactNativeHost.java
 
 # Pod install
-if [ -z "$APPCENTER_BUILD_ID" ]; then
+if [ ! -z "$APPCENTER_BUILD_ID" ]; then
   (cd ios; pod install; cd -)
-else
-  (cd ios; pod install --repo-update; cd -)
 fi
