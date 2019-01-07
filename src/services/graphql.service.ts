0565177f509be8c56b13e37c2f21b9115d587f7d
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2019-01-07 15:48:38 -0500

    Defaults to persistance

--- src/services/graphql.service.ts
+++ src/services/graphql.service.ts
@@ -1,16 +1,21 @@
-import ApolloClient, { gql } from 'apollo-boost';
+import { AsyncStorage } from 'react-native';
+import { ApolloClient, InMemoryCache, HttpLink, gql } from 'apollo-boost';
+import { persistCache } from 'apollo-cache-persist';
 
-const client = new ApolloClient({
-  uri: 'https://api.graph.cool/simple/v1/cj65psnfa0oqd0187a705fd4h',
-  fetchOptions: {
-    credentials: 'include',
-  },
-  onError: ({ graphQLErrors, networkError }) => {
-    // tslint:disable-next-line no-console
-    if (graphQLErrors) return console.error('GrahQL Errors:', graphQLErrors);
-    // tslint:disable-next-line no-console
-    if (networkError) return console.error('Network Error: ', networkError);
-  },
+const cache = new InMemoryCache();
+
+persistCache({
+  cache,
+  storage: AsyncStorage,
+  maxSize: 1024 * 1024 * 4,
 });
 
-export { client, gql };
+const client = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: 'https://api.graph.cool/simple/v1/cj65psnfa0oqd0187a705fd4h',
+    credentials: 'include',
+  }),
+});
+
+export { client, cache, gql };
